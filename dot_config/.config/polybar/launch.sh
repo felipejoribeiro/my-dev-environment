6f54900e6415a625a4ad96573560d5c0188f51d6
felipejoribeiro <feliperibeiro.ufu@gmail.com>
2021-10-14 16:23:53 -0300

    configured multiple monitors

--- dot_config/.config/polybar/launch.sh
+++ dot_config/.config/polybar/launch.sh
@@ -1,6 +1,9 @@
 #!/bin/bash
 # This file configures polybar
 
+# Check what computer is this
+THIS_MACHINE=$HOSTNAME
+
 # Terminate already running bar instances
 killall -q polybar
 
@@ -9,17 +12,32 @@ while pgrep -u $UID -x polybar >/dev/null; do sleep 1; done
 
 # set up monitors for polybar, platform agnostic
 echo "configuring polybar for multiple monitors..."
-for m in $(xrandr --query | grep " connected" | cut -d" " -f1); do
-	if [[ $m = *HDMI-0* ]]; then
-		MONITOR=$m polybar --reload mybar_desk &
-		echo $m polybar --reload mybar_desk &
-	elif [[ $m = *eDP1* ]]; then
-		MONITOR=$m polybar --reload mybar_note &
-		echo $m polybar --reload mybar_note &
-	else
-		MONITOR=$m polybar --reload external &
-		echo $m polybar --reload external &
-	fi
-done
+
+if [[ $THIS_MACHINE == *"redbox"* ]]; then
+	for m in $(xrandr --query | grep " connected" | cut -d" " -f1); do
+		if [[ $m = *HDMI-1-0* ]]; then
+			MONITOR=$m polybar --reload mybar_desk &
+			echo $m polybar --reload mybar_desk &
+		elif [[ $m = *eDP1* ]]; then
+			MONITOR=$m polybar --reload mybar_note &
+			echo $m polybar --reload mybar_note &
+		else
+			MONITOR=$m polybar --reload external &
+			echo $m polybar --reload external &
+		fi
+	done
+fi
+
+if [[ $THIS_MACHINE == *"redspace"* ]]; then
+	for m in $(xrandr --query | grep " connected" | cut -d" " -f1); do
+		if [[ $m = *HDMI-0* ]]; then
+			MONITOR=$m polybar --reload mybar_desk &
+			echo $m polybar --reload mybar_desk &
+		else
+			MONITOR=$m polybar --reload external &
+			echo $m polybar --reload external &
+		fi
+	done
+fi
 
 echo "Polybar launched..."
--- user_binaries/.local/bin/switch_monitor.sh
+++ user_binaries/.local/bin/switch_monitor.sh
@@ -1,22 +1,29 @@
 #!/bin/bash
 
-# set up the two monitors for bspwm
+# set up the two monitors for bspwm platform independent
 my_laptop_external_monitor=$(xrandr --query | grep 'HDMI-1-0')
-if [[ $my_laptop_external_monitor = *disconnected* ]]; then
-	$HOME/.screenlayout/screen_one.sh
-	bspc monitor eDP1 -d 1 2 3 4 5 6
-else
-	if [[ $my_laptop_external_monitor = *connected* ]]; then
-		$HOME/.screenlayout/with_wacom.sh
-		bspc monitor HDMI-1-0 -d 1 2
-		bspc monitor eDP1 -d 3 4 5 6
-		bspc monitor HDMI-1-0 -s eDP1 
-	else
-		$HOME/.screenlayout/screen_one.sh
-		bspc monitor eDP1 -d 1 2 3 4 5 6
-	fi
+this_machine=$HOSTNAME
+
+if [[ $this_machine = *redspace* ]]; then
+	bspc monitor "HDMI-0" -d 1 2 3
+	bspc monitor "DP-3" -d 4 5
+	bspc monitor "DP-1" -d 6 7
+	bspc wm -O HDMI-0 DP-3 DP-1
 fi
 
+if [[ $this_machine = *redbox* ]]; then
+	if [[ $my_laptop_external_monitor = *disconnected* ]]; then
+		bspc monitor eDP1 -d 1 2 3 4 5 6
+	else
+		if [[ $my_laptop_external_monitor = *connected* ]]; then
+			bspc monitor HDMI-1-0 -d 1 2 3
+			bspc monitor eDP1 -d 4 5 6
+			bspc wm -O HDMI-1-0 eDP1
+		else
+			bspc monitor eDP1 -d 1 2 3 4 5 6
+		fi
+	fi
+fi
 
 # relaunch polybar
 $HOME/.config/polybar/launch.sh &
