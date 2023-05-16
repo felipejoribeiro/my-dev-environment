30b0c3fb84be4168ef60127824f176d5261cc08e
felipejoribeiro <feliperibeiro.ufu@gmail.com>
2023-05-16 00:46:31 -0300

    better godot-nvim

--- user_binaries/.local/bin/godot-nvim.sh
+++ user_binaries/.local/bin/godot-nvim.sh
@@ -3,10 +3,19 @@
 # like with the command below:
 # ln -s /usr/bin/nvim /usr/bin/nvim-godot
 
-## echo all env variables
 [ -n "$1" ] && file=$1
-if ! pgrep -x "nvim-godot" > /dev/null; then
-  alacritty -e nvim-godot 
-  alacritty -e zsh -c "source /home/fejori/.zshrc && nvim-godot --listen ~/.cache/nvim/godot.pipe $file"
+[ -n "$2" ] && col=$2
+[ -n "$3" ] && line=$3
+is_nvim_godot_running=$(pgrep -x 'nvim-godot')
+
+if [ -n "$is_nvim_godot_running" ]; then
+  nvim-godot --server ~/.cache/nvim/godot.pipe --remote-send ':e '$file'<CR>'
+else
+  if [ -z "$line" ]; then
+    alacritty -e zsh -c "source /home/fejori/.zshrc && nvim-godot --listen ~/.cache/nvim/godot.pipe $file"
+  elif [ -z "$col" ]; then
+    alacritty -e zsh -c "source /home/fejori/.zshrc && nvim-godot +'call cursor($line,1)' --listen ~/.cache/nvim/godot.pipe $file"
+  else
+    alacritty -e zsh -c "source /home/fejori/.zshrc && nvim-godot +'call cursor($line,$col)' --listen ~/.cache/nvim/godot.pipe $file"
+  fi
 fi
-nvim-godot --server ~/.cache/nvim/godot.pipe --remote-send ':e '$file'<CR>'
